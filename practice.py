2c75e41b11f8ef9f7c5805559ed5c1a0bcb58aca
sun-yoon-e <shinee252525@icloud.com>
2019-10-07 01:09:24 +0900

    Week02
    리소스 수집이 더 필요할 것 같음
    화면에 배경 꽉차게 그림 놓기까지 완료
    배경 움직이기를 목표로 했으나 실패

--- practice.py
+++ practice.py
@@ -1,13 +1,18 @@
 from pico2d import*
-open_canvas()
+open_canvas(1266, 712)
 background = load_image('background.png')
 character = load_image('s3.png')
 
 x = 0
+b1 = 633
+b2 = 0
 
 while x < 800:
     clear_canvas()
-    background.draw(400, 350)
+    background.draw(b1, 356)
+    b1 += 3
+    background.draw(b2, 356)
+    b2 += 3
     character.draw(x, 90)
     update_canvas()
     x += 5
