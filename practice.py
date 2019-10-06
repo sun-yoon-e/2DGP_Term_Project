eeac229332791ac56e729ae9a152a22d1f19ba00
sun-yoon-e <shinee252525@icloud.com>
2019-10-07 03:11:09 +0900

    Week02
    키입력시 캐릭터 점프 구현 추가

--- practice.py
+++ practice.py
@@ -1,4 +1,14 @@
 from pico2d import*
+
+def mouse_event():
+    global frame
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDL_KEYUP:
+            character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
+            update_canvas()
+            frame = (frame + 1) % 4
+
 open_canvas(800, 107)
 background = load_image('back.png')
 character = load_image('char.png')
@@ -11,7 +21,7 @@ while True:
     clear_canvas()
     background.draw(b1, 80)
     background.draw(b2, -30)
-    character.clip_draw(frame* 35, 0, 35, 40, 20, 20)
+    character.clip_draw(frame* 35, 45, 35, 40, 20, 20)
     update_canvas()
     frame = (frame + 1) % 6
     b1 -= 3
