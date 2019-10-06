a1132a6cd8bf9294facbd0787c6edf6c736bf736
sun-yoon-e <shinee252525@icloud.com>
2019-10-07 02:49:51 +0900

    Week02
    화면에 배경추가
    화면에 캐릭터추가
    배경 이동 구현
    캐릭터 움직임 구현

--- practice.py
+++ practice.py
@@ -1,21 +1,21 @@
 from pico2d import*
-open_canvas(1266, 712)
-background = load_image('background.png')
-character = load_image('s3.png')
+open_canvas(800, 107)
+background = load_image('back.png')
+character = load_image('char.png')
 
-x = 0
+frame = 0
 b1 = 633
-b2 = 0
+b2 = 633
 
-while x < 800:
+while True:
     clear_canvas()
-    background.draw(b1, 356)
-    b1 += 3
-    background.draw(b2, 356)
-    b2 += 3
-    character.draw(x, 90)
+    background.draw(b1, 80)
+    background.draw(b2, -30)
+    character.clip_draw(frame* 35, 0, 35, 40, 20, 20)
     update_canvas()
-    x += 5
+    frame = (frame + 1) % 6
+    b1 -= 3
+    b2 -= 3
     delay(0.05)
     get_events()
 
