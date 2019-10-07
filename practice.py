13e18af6aacccab4e6dec1183161c048124ae123
sun-yoon-e <shinee252525@icloud.com>
2019-10-08 02:19:34 +0900

    Week02
    키입력이 왜 안되는 걸까
    마우스 입력 대신 키입력으로 바꿈

--- practice.py
+++ practice.py
@@ -1,32 +1,40 @@
 from pico2d import*
 
-def mouse_event():
+def key_event():
     global frame
+    global playing
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN and event.key == SDL_KEYUP:
-            character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
-            update_canvas()
-            frame = (frame + 1) % 4
+        if event.type == SDL_QUIT:
+            playing = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                playing = False
+            elif event.key == SDLK_UP:
+                character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
+                update_canvas()
+                frame = (frame + 1) % 4
+    pass
 
 open_canvas(800, 107)
 background = load_image('back.png')
 character = load_image('char.png')
 
+playing = True
 frame = 0
 b1 = 633
 b2 = 633
 
-while True:
+while playing:
     clear_canvas()
     background.draw(b1, 80)
     background.draw(b2, -30)
     character.clip_draw(frame* 35, 45, 35, 40, 20, 20)
     update_canvas()
     frame = (frame + 1) % 6
-    b1 -= 3
-    b2 -= 3
-    delay(0.05)
+    b1 -= 1
+    b2 -= 1
+    delay(0.03)
     get_events()
 
 close_canvas()
