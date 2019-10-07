7d785287cd1030443e2a043514dca5180ab7ade2
sun-yoon-e <shinee252525@icloud.com>
2019-10-08 03:05:13 +0900

    Week02
    점프구현 완성된 것 같음
    근데 점프 안해도 계속 달리고 있어야 하는데 화면에 나타나질 않는다 ㅠㅠ

--- practice.py
+++ practice.py
@@ -2,8 +2,7 @@ from pico2d import*
 
 def key_event():
     frame = 0
-    b1 = 633
-    b2 = 633
+    global b1, b2
 
     events = get_events()
     for event in events:
@@ -12,18 +11,11 @@ def key_event():
                 character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
                 update_canvas()
                 frame = (frame + 1) % 4
-                delay(0.05)
-        elif event.key == SDL_KEYUP:
-            clear_canvas()
-            background.draw(b1, 80)
-            background.draw(b2, -30)
+                delay(0.03)
+        else:
             character.clip_draw(frame * 35, 45, 35, 40, 20, 20)
             update_canvas()
-
             frame = (frame + 1) % 6
-            b1 -= 1
-            b2 -= 1
-
             delay(0.03)
     pass
 
@@ -32,6 +24,17 @@ open_canvas(800, 107)
 background = load_image('back.png')
 character = load_image('char.png')
 
-key_event()
+b1, b2 = 633, 633
+while True:
+    clear_canvas()
+    background.draw(b1, 80)
+    background.draw(b2, -30)
+
+    update_canvas()
+    b1 -= 1
+    b2 -= 1
+    delay(0.03)
+
+    key_event()
 
 close_canvas()
