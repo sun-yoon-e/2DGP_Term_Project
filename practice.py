de76016f7f4d7530ba14bebf37cc8abc3358184e
sun-yoon-e <shinee252525@icloud.com>
2019-10-08 02:51:32 +0900

    Week02
    종료버튼 없애고 키입력 함수로 몽땅 보냄

--- practice.py
+++ practice.py
@@ -1,46 +1,37 @@
 from pico2d import*
 
 def key_event():
-    global frame
-    global playing
+    frame = 0
+    b1 = 633
+    b2 = 633
 
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
-            playing = False
-
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                playing = False
-
-            elif event.key == SDLK_SPACE:
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_SPACE:
                 character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
                 update_canvas()
                 frame = (frame + 1) % 4
                 delay(0.05)
+        elif event.key == SDL_KEYUP:
+            clear_canvas()
+            background.draw(b1, 80)
+            background.draw(b2, -30)
+            character.clip_draw(frame * 35, 45, 35, 40, 20, 20)
+            update_canvas()
+
+            frame = (frame + 1) % 6
+            b1 -= 1
+            b2 -= 1
+
+            delay(0.03)
     pass
 
 open_canvas(800, 107)
+
 background = load_image('back.png')
 character = load_image('char.png')
 
-playing = True
-frame = 0
-b1 = 633
-b2 = 633
-
-while playing:
-    clear_canvas()
-    background.draw(b1, 80)
-    background.draw(b2, -30)
-    character.clip_draw(frame* 35, 45, 35, 40, 20, 20)
-    update_canvas()
-
-    key_event()
-    frame = (frame + 1) % 6
-    b1 -= 1
-    b2 -= 1
-
-    delay(0.03)
+key_event()
 
 close_canvas()
