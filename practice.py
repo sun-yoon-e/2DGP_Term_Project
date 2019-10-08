e2f6f4a6c0ad29d6180444db403058d45c22a928
sun-yoon-e <shinee252525@icloud.com>
2019-10-09 04:49:36 +0900

    Week02
    맵 크기 조정
    캐릭터 점프 구현 잘 안됨 ㅠ
    맵이 점점 더 작아져서 고민,,

--- practice.py
+++ practice.py
@@ -1,36 +1,38 @@
 from pico2d import*
 
 def key_event():
-    frame = 0
+    global frame
     global b1, b2
 
     events = get_events()
     for event in events:
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_SPACE:
+                #update_canvas()
                 character.clip_draw(frame * 35, 0, 35, 40, 20, 40)
                 update_canvas()
                 frame = (frame + 1) % 4
                 delay(0.03)
-        else:
-            character.clip_draw(frame * 35, 45, 35, 40, 20, 20)
-            update_canvas()
-            frame = (frame + 1) % 6
-            delay(0.03)
+
     pass
 
-open_canvas(800, 107)
+open_canvas(800, 75)
 
-background = load_image('back.png')
+background = load_image('p1.png')
 character = load_image('char.png')
 
+frame = 0
 b1, b2 = 633, 633
+
 while True:
     clear_canvas()
-    background.draw(b1, 80)
+    background.draw(b1, 50)
     background.draw(b2, -30)
 
+    character.clip_draw(frame * 35, 45, 35, 40, 20, 20)
     update_canvas()
+
+    frame = (frame + 1) % 6
     b1 -= 1
     b2 -= 1
     delay(0.03)
