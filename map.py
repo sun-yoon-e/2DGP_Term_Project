6e56f7f54d788d70c93ee295775eb8c7351f32f3
sun-yoon-e <shinee252525@icloud.com>
2019-10-10 03:11:42 +0900

    Week02
    맵 이어붙이기 내일 다시

--- map.py
+++ map.py
@@ -2,25 +2,30 @@ from pico2d import*
 
 open_canvas(1500, 780)
 
-background = load_image('p1.png')
-
-frame = 0
-b1, b2, b3 = 2883, 2883, 2883
+map1 = load_image('p1.png')
+map2 = load_image('p2.png')
+b1, b2 = 2883, 5764
 
 while True:
-    while b1 > - 2882:
+    if b1 > -5000:
         clear_canvas()
-        background.draw(0, 0)
-        background.draw(b1, 1182)
-        background.draw(b2, 390)
-        background.draw(b3, -400)
+        map1.draw(b1, 1182)
+        map1.draw(b1, 390)
+        map1.draw(b1, -400)
 
         update_canvas()
-
-        frame = (frame + 1) % 6
-        b1 -= 3
-        b2 -= 3
-        b3 -= 3
+        b1 -= 30
         delay(0.03)
+
+        if b1 <= -1264 and b2 > -2882:
+            clear_canvas()
+            map2.draw(b2, 1182)
+            map2.draw(b2, 390)
+            map2.draw(b2, -400)
+
+            update_canvas()
+            b2 -= 3
+            delay(0.03)
+
 
 close_canvas()
