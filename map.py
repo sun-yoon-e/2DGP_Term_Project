d84736aa748fd85d3d012e70bd47be47706093d3
sun-yoon-e <shinee252525@icloud.com>
2019-10-11 03:43:17 +0900

    Week02
    맵 4개 이어붙이기 성공
    하지만 맵 두개가 같이 나올 때 깜빡거리는 문제 수정해야함

--- map.py
+++ map.py
@@ -4,28 +4,49 @@ open_canvas(1500, 780)
 
 map1 = load_image('p1.png')
 map2 = load_image('p2.png')
-b1, b2 = 2883, 5764
+map3 = load_image('p3.png')
+map4 = load_image('p4.png')
+b1, b2, b3, b4 = 2882, 4382, 4382, 4382
 
 while True:
-    if b1 > -5000:
+    if b1 > -2882:
         clear_canvas()
         map1.draw(b1, 1182)
         map1.draw(b1, 390)
         map1.draw(b1, -400)
 
         update_canvas()
-        b1 -= 30
+        b1 -= 70
         delay(0.03)
 
-        if b1 <= -1264 and b2 > -2882:
-            clear_canvas()
-            map2.draw(b2, 1182)
-            map2.draw(b2, 390)
-            map2.draw(b2, -400)
+    if b1 <= -1382 and b2 > -2882:
+        clear_canvas()
+        map2.draw(b2, 1182)
+        map2.draw(b2, 390)
+        map2.draw(b2, -400)
 
-            update_canvas()
-            b2 -= 3
-            delay(0.03)
+        update_canvas()
+        b2 -= 70
+        delay(0.03)
 
+    if b2 <= -1382 and b3 > -2882:
+        clear_canvas()
+        map3.draw(b3, 1182)
+        map3.draw(b3, 390)
+        map3.draw(b3, -400)
+
+        update_canvas()
+        b3 -= 70
+        delay(0.03)
+
+    if b3 <= -1382 and b4 > -2882:
+        clear_canvas()
+        map4.draw(b4, 1182)
+        map4.draw(b4, 390)
+        map4.draw(b4, -400)
+
+        update_canvas()
+        b4 -= 70
+        delay(0.03)
 
 close_canvas()
