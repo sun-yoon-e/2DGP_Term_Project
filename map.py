a4cb48fc2a985d2c25e2d4fe077e2db38a281e3b
sun-yoon-e <shinee252525@icloud.com>
2019-10-20 19:52:51 +0900

    Week03
    맵 띄우기 전부 뜯어 고침
    Week04 에는 그동안 못한 맵, 캐릭터 전부 해야할 것

--- map.py
+++ map.py
@@ -1,19 +1,30 @@
 from pico2d import*
 
-open_canvas(1441, 320)
+open_canvas(1200, 290)
 
 map1 = load_image('map1.png')
 map2 = load_image('map2.png')
 map3 = load_image('map3.png')
 map4 = load_image('map4.png')
-b1, b2, b3, b4 = 1441, 2182, 2189, 2216
+b1, b2 = 1441, 2882
 
 while True:
     clear_canvas()
-    map1.draw(b1, 0)
+    map1.clip_draw(0, 0, 2882, 350, b1, 145)
+    map1.clip_draw(0, 600, 2882, 365, b1, 120)
+    map1.clip_draw(0, 305, 2882, 340, b1, 150)
+
+    if b1 <= -241 and b2 >= 1441:
+        map1.clip_draw(0, 0, 2882, 350, b1, 145)
+        map1.clip_draw(0, 600, 2882, 365, b1, 120)
+        map1.clip_draw(0, 305, 2882, 340, b1, 150)
+        map2.clip_draw(0, 0, 2882, 350, b2, 150)
+        update_canvas()
 
     update_canvas()
-    #b1 -= 50
-   # delay(0.03)
+    b1 -= 30
+    b2 -= 30
+    delay(0.03)
+
 
 close_canvas()
