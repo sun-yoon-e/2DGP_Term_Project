c664ec2e19e6e9e6cfb48a72a7f237f1cdeb3622
sun-yoon-e <shinee252525@icloud.com>
2019-10-20 20:23:23 +0900

    Week03

--- map.py
+++ map.py
@@ -8,23 +8,22 @@ map3 = load_image('map3.png')
 map4 = load_image('map4.png')
 b1, b2 = 1441, 2882
 
-while True:
+while b1 >= -1441:
     clear_canvas()
     map1.clip_draw(0, 0, 2882, 350, b1, 145)
     map1.clip_draw(0, 600, 2882, 365, b1, 120)
     map1.clip_draw(0, 305, 2882, 340, b1, 150)
-
-    if b1 <= -241 and b2 >= 1441:
-        map1.clip_draw(0, 0, 2882, 350, b1, 145)
-        map1.clip_draw(0, 600, 2882, 365, b1, 120)
-        map1.clip_draw(0, 305, 2882, 340, b1, 150)
-        map2.clip_draw(0, 0, 2882, 350, b2, 150)
-        update_canvas()
-
     update_canvas()
     b1 -= 30
     b2 -= 30
     delay(0.03)
 
+while b1 <= -241 and b2 <= -241:
+    map2.clip_draw(0, 300, 2882, 330, b2, 160)
+    map2.clip_draw(0, 600, 2882, 400, b2, 120)
+    update_canvas()
+    b1 -= 30
+    b2 -= 30
+    delay(0.03)
 
 close_canvas()
