98d4ba39f7a12e2b19e657add630984705a2255d
sun-yoon-e <shinee252525@icloud.com>
2019-10-27 03:16:25 +0900

    Week04
    클래스 완료

--- character.py
+++ character.py
@@ -6,7 +6,7 @@ background = None
 
 class Character:
     def __init__(self):
-        self.x, self.y = 100, 70
+        self.x, self.y = 100, 50
         self.frame = 0
         self.image = load_image('character.png')
 
@@ -44,18 +44,15 @@ class Background:
         if self.x1 <= 0 and self.x2 >= -1441:
             self.image2.clip_draw(0, 300, 2882, 330, self.x2, 160)
             self.image2.clip_draw(0, 600, 2882, 400, self.x2, 120)
-            self.x2 -= 5
 
         if self.x2 <= 0 and self.x3 >= -1441:
             self.image3.clip_draw(0, 300, 2882, 330, self.x3, 310)
             self.image3.clip_draw(0, 500, 2882, 325, self.x3, 160)
-            self.x3 -= 5
 
         if self.x3 <= 0 and self.x4 >= -1441:
             self.image4.clip_draw(0, 0, 2882, 350, self.x4, 145)
             self.image4.clip_draw(0, 600, 2882, 340, self.x4, 100)
             self.image4.clip_draw(0, 305, 2882, 330, self.x4, 160)
-            self.x4 -= 5
 
 
 def enter():
@@ -89,7 +86,7 @@ def update():
 
 def draw():
     clear_canvas()
-    character.draw()
     background.draw()
+    character.draw()
     update_canvas()
     delay(0.03)
---
+++ spongebob_sqaurepants.py
@@ -0,0 +1,7 @@
+import game_framework
+import pico2d
+import character
+
+pico2d.open_canvas(800,290)
+game_framework.run(character)
+pico2d.close_canvas()
