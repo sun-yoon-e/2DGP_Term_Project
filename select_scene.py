5f1e545de81f430e5d853553686f53a6a7752483
sun-yoon-e <shinee252525@icloud.com>
2019-11-08 23:04:51 +0900

    Week06
    캐릭터 선택화면에서 마우스 버튼 누르는거

--- select_scene.py
+++ select_scene.py
@@ -31,8 +31,13 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(game_main)
+            #elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                #game_framework.change_state(game_main)
+            elif (event.type, event.button) == (SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT):
+                if (250 < event.x < 350 and 20 < event.y < 140):
+                    pass
+                elif (450 < event.x < 550 and 20 < event.y < 160):
+                    pass
 
 
 def update():
