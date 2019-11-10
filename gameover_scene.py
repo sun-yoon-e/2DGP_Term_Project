567491414d45884940c885c4f5be8c6f6d5829de
sun-yoon-e <shinee252525@icloud.com>
2019-11-10 23:47:38 +0900

    Week06
    게임오버 신 추가

---
+++ gameover_scene.py
@@ -0,0 +1,53 @@
+import game_framework
+from pico2d import *
+
+over = None
+
+
+class Over:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+def enter():
+    global over
+    over = Over()
+
+
+def exit():
+    global pause
+    del pause
+
+
+def update():
+    global over
+    over.update()
+
+
+def draw():
+    global over
+    clear_canvas()
+    over.update()
+    over.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        pass
+
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
