cc62c2f56743f6e9fc25f439b86d72ea45f039eb
sun-yoon-e <shinee252525@icloud.com>
2019-11-12 22:53:24 +0900

    Week07
    화면 넘어가는 부분 다듬기

--- pause_scene.py
+++ pause_scene.py
@@ -4,6 +4,7 @@ from pico2d import *
 
 pause = None
 
+
 class Pause:
     def __init__(self):
         self.image_back = load_image('resource/@Using/pause.png')
@@ -14,8 +15,8 @@ class Pause:
         pass
 
     def draw(self):
-        #self.image_back.draw(400, 145, 800, 290)
-        self.image_back.composite_draw(3.141592 / 2, '', 400, 145, 290, 800)
+        #self.image_back.draw(400, 145, 800, 290) #회전 x
+        self.image_back.composite_draw(3.141592 / 2, '', 400, 145, 290, 800) #회전 o
         self.image_continue.draw(400, 210)
         self.image_quit.draw(400, 70)
 
@@ -48,14 +49,11 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        #elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            #game_framework.pop_state()
         elif (event.type, event.button) == (SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT):
             if 100 < event.x < 700 and 170 < 290 - 1 - event.y < 250:
                 game_framework.pop_state()
             elif 250 < event.x < 550 and 30 < 290 - 1 - event.y < 110:
                 game_framework.change_state(gameover_scene)
-
 
 
 def pause():
