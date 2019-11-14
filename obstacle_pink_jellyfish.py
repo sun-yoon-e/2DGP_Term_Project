ef98582b0b4f41582e3a525b5b7f5910d145939b
sun-yoon-e <shinee252525@icloud.com>
2019-11-14 23:54:29 +0900

    Update obstacle_pink_jellyfish.py

--- obstacle_pink_jellyfish.py
+++ obstacle_pink_jellyfish.py
@@ -25,3 +25,4 @@ class Pink_Jellyfish:
         self.image.clip_draw(self.frame * 30, 0, 30, 35, self.x, self.y)
         if self.x <= 800:
             self.image.clip_draw(self.frame * 30, 0, 30, 35, self.x + self.random_x + 300, self.y)
+
