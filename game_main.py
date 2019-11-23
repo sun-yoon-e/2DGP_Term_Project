371da689451fd0c25999664639637bca643a4c4a
sun-yoon-e <shinee252525@icloud.com>
2019-11-23 21:33:20 +0900

    Week08
    성공씬 수치 조정

--- game_main.py
+++ game_main.py
@@ -201,8 +201,8 @@ def update():
         game_world.remove_object(item_mr_krab)
 
         global  Success_time
-        Success_time += 0.01
-        if Success_time >= 2.0:
+        Success_time += 0.1
+        if Success_time >= 5.0:
             game_framework.change_state(game_success_scene)
 
 
